d52e1d45ceca580298f57ed589f1e33ac747f789
Sohail Shabbir <unknownperson23414@gmail.com>
2026-08-08 00:40:45 +0500

    adding the latest pacakges of the tailwind and add css

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,17 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-  }
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
